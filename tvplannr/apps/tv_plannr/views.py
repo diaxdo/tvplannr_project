22313c476bd256cf1a9099cb87867800998541c2
Dalia <dalia.icedo@gmail.com>
2017-02-26 23:08:49 -0800

    first commit

---
+++ tvplannr/apps/tv_plannr/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+#CONTROLLER
+# Create your views here.
+def index(request):
+    print('8'*100)
+    return render(request, 'tv_plannr/index.html')
+def plannr(request):
+    print('8'*100)
+    return render(request, 'tv_plannr/plannr.html')
